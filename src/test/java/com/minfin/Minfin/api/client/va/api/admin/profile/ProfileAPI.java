18a1ca0cda1baab5beed9085a4cc6e013f68b9e2
a.zinchuk <13nokia13>
2021-09-26 22:21:05 +0300

    add doc

--- src/test/java/com/minfin/Minfin/api/client/va/api/admin/profile/ProfileAPI.java
+++ src/test/java/com/minfin/Minfin/api/client/va/api/admin/profile/ProfileAPI.java
@@ -1,7 +1,9 @@
 package com.minfin.Minfin.api.client.va.api.admin.profile;
 
+import com.minfin.Minfin.api.model.minfin.api.auth.auction.AuctionResponse;
 import com.minfin.Minfin.api.model.va.api.admin.profile.ProfileRequest;
 import com.minfin.Minfin.api.model.va.api.admin.profile.ProfileResponse;
+import com.minfin.Minfin.api.model.va.api.auth.userInfo.UserInfoResponse;
 import retrofit2.Call;
 import retrofit2.http.*;
 
@@ -10,6 +12,8 @@ public interface ProfileAPI {
     @Headers({"Cookie:minfin_sessions=a7497b06881fa70bc5bc7ed6ca0c6387a1c6b781",
             "Content-Type: application/json"})
     @POST("api/admin/profile/{profileId}/subscription")
+    /** @param profileId @see {@link UserInfoResponse#profileId }
+     * @param bearerToken @see {@link AuctionResponse#accessToken}*/
     Call<ProfileResponse> postProfile(@Path("profileId") String profileId,
                                       @Header(value = "Authorization") String bearerToken,
                                       @Body ProfileRequest body);
